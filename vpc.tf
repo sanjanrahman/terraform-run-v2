69fead57718087cb96a07c20cef5decfc1b1cf89
Sanjan Rahman <sanjanrahman@gmail.com>
2020-03-30 02:36:52 -0500

    take off auth

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,6 @@
 # vpc.tf 
 # Create VPC/Subnet/Security Group/Network ACL
 provider "aws" { 
- access_key = var.access_key
- secret_key = var.secret_key 
  region     = var.region
 }
 # create the VPC
